338ad751d7f2883cac2ab0ecf4a5d007ed3b5b19
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-08-21 15:52:54 +0700

    ADD gulp protecter

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 var gulp = require('gulp');
+var protractor = require("gulp-protractor").protractor;
+var protractor_conf = require('./e2e-tests/protractor.conf');
 
 gulp.task('default', function() {
+});
+
+gulp.task('e2e-test', function() {
+  gulp.src(protractor_conf.config.specs)
+    .pipe(protractor({
+      configFile: "e2e-tests/protractor.conf.js",
+      args: ['--baseUrl', 'http://127.0.0.1:8000']
+    }))
+    .on('error', function(e) { throw e });
 });
